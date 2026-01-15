eaa9617e0ca93a8b245b7ee6e4f32ef8b9a438ea
rusu-sebastian808 <rususebastian808@gmail.com>
2026-01-15 16:01:49 +0200

    Update Cart.php

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -12,25 +12,15 @@ class Cart extends Model
         'quantity'
     ];
 
-    /**
-     * Get the user that owns the cart item
-     */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    /**
-     * Get the game that owns the cart item
-     */
     public function game()
     {
         return $this->belongsTo(Game::class);
     }
-
-    /**
-     * Get the total price for this cart item
-     */
     public function getTotalPriceAttribute()
     {
         return $this->quantity * $this->game->price;
