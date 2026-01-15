331a278ac69d52bedfb3eca7a542a0ddc969e659
rusu-sebastian808 <rususebastian808@gmail.com>
2026-01-15 16:19:00 +0200

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -9,12 +9,10 @@ use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
     public function run(): void
     {
-        // Create admin user
+ 
         User::create([
             'name' => 'Admin User',
             'email' => 'admin@example.com',
@@ -23,7 +21,7 @@ class UserSeeder extends Seeder
             'email_verified_at' => now(),
         ]);
 
-        // Create customer user
+     
         User::create([
             'name' => 'Customer User',
             'email' => 'customer@example.com',
